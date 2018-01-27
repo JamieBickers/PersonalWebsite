059ab32b88a8036e463a4c7690faffcdee746356
JamieBickers <bickersjamie@googlemail.com>
2018-01-27 19:20:57 +0000

    Minor wording changes.

--- DeployPersonalWebsite.sh
+++ DeployPersonalWebsite.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ -z "$1" ]
+then
+	echo "Need a commit message!"
+	exit 1
+fi
+git add .
+git commit -m "$1"
+git push
 dotnet publish -c Release
 docker build -t jamie-bickers-personal-website ./PersonalWebsite/bin/release/netcoreapp2.0/publish
 docker tag jamie-bickers-personal-website registry.heroku.com/jamie-bickers-personal-website/web
