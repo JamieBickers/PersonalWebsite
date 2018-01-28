6a26a2a7647b944dc6d0d2b00f42e11953853ec3
JamieBickers <bickersjamie@googlemail.com>
2018-01-28 19:05:12 +0000

    Added password memory and hiding.

--- DeployPersonalWebsite.sh
+++ DeployPersonalWebsite.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-if [ -z "$1" ]
+if ! [ -z "$1" ]
 then
-	echo "Need a commit message!"
-	exit 1
+	git add -A .
+	git commit -m "$1"
+	git push
 fi
-git add -A .
-git commit -m "$1"
-git push
 dotnet publish -c Release
 docker build -t jamie-bickers-personal-website ./PersonalWebsite/bin/release/netcoreapp2.0/publish
 docker tag jamie-bickers-personal-website registry.heroku.com/jamie-bickers-personal-website/web
